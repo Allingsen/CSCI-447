25952a4ce6576b85442fa3bc9725a32cde45296d
Kellen <142949567+Hurlicane24@users.noreply.github.com>
2024-09-09 17:59:24 -0600

    updated ModelTest.py
    I added 0/1-loss and recall

--- Project_1/ModelTest.py
+++ Project_1/ModelTest.py
@@ -1,5 +1,6 @@
 #ModelTest inherits from NaiveBayesModel
 import pandas as pd
+import numpy as np
 from NaiveBayesModel import NaiveBayesModel
 
 #--------------------------------------------------------------------------------------------------------------------------
@@ -30,9 +31,10 @@ class ModelTest(NaiveBayesModel):
     
 #--------------------------------------------------------------------------------------------------------------------------
 
-    #Classifies all feature vectors in test_data
+    #Classifies all feature vectors in test_data and returns a list of the classifications
     def classify_all(self):
         print("\nPredicted Classes for Test Set:\n-------------------------------")
+        predicted_classes = []
         for i in range(len(self.test_data)):
             feature_vector = {}
             for column in self.test_data.columns:
@@ -40,7 +42,47 @@ class ModelTest(NaiveBayesModel):
                     feature_vector[column] = self.test_data.loc[i, column]
             classification = self.classify_one(feature_vector)
             print("Vector {}: Class {}".format(list(feature_vector.values()), classification))
+            predicted_classes.append(classification)
+        return(predicted_classes)
 
+#--------------------------------------------------------------------------------------------------------------------------
+
+    #Returns the 0/1-Loss of the learner over the test set (Works on multi-class problems)
+    def zero_one_loss(self, predicted_classes):
+        loss = 0
+        for i in range(len(self.test_data)):
+            if(self.test_data.loc[i, "class"] != predicted_classes[i]):
+                loss += 1
+        return(loss)
+    
+#--------------------------------------------------------------------------------------------------------------------------
+
+    #Returns the recall (Works on multi-class problems)
+    def recall(self, predicted_classes):
+
+        #confusion_total represents the TPs, TNs, FPs, and FNs summed across classes
+        confusion_total = np.zeros(4)
+        for Class in self.classes:
+
+            #confusion_local represents the TPs, TNs, FPs, and FNs for a specific class
+            confusion_local = np.zeros(4)
+            for i in range(len(self.test_data)):
+                if(self.test_data.loc[i, "class"] == Class and predicted_classes[i] == Class):
+                    confusion_local[0] += 1
+                elif(self.test_data.loc[i, "class"] != Class and predicted_classes[i] != Class):
+                    confusion_local[1] += 1
+                elif(self.test_data.loc[i, "class"] != Class and predicted_classes[i] == Class):
+                    confusion_local[2] += 1
+                elif(self.test_data.loc[i, "class"] == Class and predicted_classes[i] != Class):
+                    confusion_local[3] += 1
+
+            for i in range(4):
+                confusion_total[i] += confusion_local[i]
+
+        #Recall = TPs/(TPs + FNs)
+        recall = confusion_total[0]/(confusion_total[0] + confusion_total[3])
+        return(recall)
+             
 #--------------------------------------------------------------------------------------------------------------------------
 
 test_data = {
@@ -66,4 +108,5 @@ print("\nTraining Set\n------------\n", training_df)
 test = ModelTest(training_df, test_df)
 test.load_data()
 test.train_model()
-test.classify_all()
+predicted_classes = test.classify_all()
+print("\nPerformance Metrics\n-------------------\n0/1-Loss = {}\nRecall = {}".format(test.zero_one_loss(predicted_classes), test.recall(predicted_classes)))
